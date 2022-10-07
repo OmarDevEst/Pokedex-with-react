7365eaa1e051b630bfbf524ca8f3bca5dc65e6f6
ckansito981@gmail.com <ckansito981@gmail.com>
2022-10-06 23:01:33 -0500

    Beta abierta 2.0v

--- src/Components/Presentacion.jsx
+++ src/Components/Presentacion.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
 import "../scss/Presentacion.scss";
+import Medalla from "../image/medalla_pesa.jpg";
 import Perfil from "../image/white-fotoperfil.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -15,40 +16,71 @@ const Presentacion = () => {
   return (
     <main className="Presentacion">
       <NavBar className="NavBar"></NavBar>
-      <div className="MaxCon">
-        <div className="RedCon">
-          <div className="Foto">
-            <img src={Perfil} alt="Imagen de perfil"></img>
-          </div>
-          <div className="datos">
-            <div className="texto">
-              <section>
-                <h1>This pokedex was devoloped for Omar Esteban Gutierrez</h1>
-              </section>
-              <section>
-                <h2>
-                  I am a programmer who enjoys designing web pages and loves
-                  video games and programming. I work in Python, C, React,
-                  Flutter. developed ,you could know me better in :
-                </h2>
-              </section>
+      <section className="Pokedex">
+        <section className="pestana_izquierda">
+          <section className="insignia">
+            <img src={Medalla} alt="Kanto"></img>
+          </section>
+          <section className="Pokemon">
+            <div>
+              <img src={Perfil} alt="Perfil"></img>
             </div>
-            <div className="Iconos">
-              <div className="iconos_con">
-                <a href={Twitter} target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} className="logo" />
-                </a>
-                <a href={Github} target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faGithub} className="logo" />
-                </a>
-                <a href={Instagram} target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faInstagram} className="logo" />
-                </a>
+          </section>
+          <section className="untipos">
+            <div className="tipo">
+              <h2>Human</h2>
+            </div>
+          </section>
+          <section className="cruzeta">
+            <div className="gridcruz">
+              <div className="griditem"></div>
+              <div className="arriba"></div>
+              <div className="griditem"></div>
+              <div className="izquierda"></div>
+              <div className="centro"></div>
+              <div className="derecha"></div>
+              <div className="griditem"></div>
+              <div className="abajo"></div>
+              <div className="griditem"></div>
+              <div />
+            </div>
+          </section>
+        </section>
+        <section className="pestana_derecha">
+          <div className="containerPantalla">
+            <div className="pantallaNegra">
+              <div className="pantalla_gris">
+                <div className="caracteristica">
+                  <h1>This pokedex was devoloped for Omar Esteban Gutierrez</h1>
+                </div>
+                <div className="numero">
+                  <h2>
+                    I am a programmer who enjoys designing web pages and loves
+                    video games and programming. I work in Python, C, React,
+                    Flutter. developed ,you could know me better in :
+                  </h2>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+          <div className="pantallaAzul">
+            <div className="iconos_con">
+              <a href={Twitter} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} className="logo" />
+              </a>
+              <a href={Github} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} className="logo" />
+              </a>
+              <a href={Instagram} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="logo" />
+              </a>
+            </div>
+          </div>
+          <div className="circleContainer">
+            <div className="circulo"></div>
+          </div>
+        </section>
+      </section>
     </main>
   );
 };
